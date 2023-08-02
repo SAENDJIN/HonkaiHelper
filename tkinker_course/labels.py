fa84f68f2d8da68335ddae124538f20f6cd3b90f
SAENDJIN <andrey.saenko31@gmail.com>
2023-08-02 18:05:10 +0300

    labels and new func

---
+++ tkinker_course/labels.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+from windows import set_dpi_awareness
+from PIL import Image, ImageTk
+
+set_dpi_awareness()
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Examples")
+
+image = Image.open("C:\\Users\\andre\\PycharmProjects\\HonkaiHelper\\img_char\\Clara.webp"). \
+    resize((128, 128))
+photo = ImageTk.PhotoImage(image)
+
+label = ttk.Label(root, text="Hello, world!", padding=20)
+label.config(font=("Segot UI", 20))
+label.pack()
+
+label_img = ttk.Label(root, text="Clara", image=photo, padding=5, compound="top")
+label_img.pack()
+
+quick_btn = ttk.Button(root, text="Quit", command=root.destroy)
+quick_btn.pack()
+
+root.mainloop()
---
+++ tkinker_course/windows.py
@@ -0,0 +1,7 @@
+def set_dpi_awareness():
+    try:
+        from ctypes import windll
+        windll.shcore.SetProcessDpiAwareness(1)
+    except:
+        pass
+
