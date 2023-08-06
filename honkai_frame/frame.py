6145ec89e9523ffec9bd01cd5b2cc3102c9c19ca
SAENDJIN <andrey.saenko31@gmail.com>
2023-08-06 23:33:48 +0300

    abc

--- honkai_frame/frame.py
+++ honkai_frame/frame.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 class HonkaiHelper(tk.Tk):
@@ -7,12 +8,10 @@ class HonkaiHelper(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("StarRail Helper")
-        self.geometry("600x800")
-        self.resizable(False, False)
         self.frames = dict()
 
         container = ttk.Frame(self)
-        container.grid(padx=10, pady=10, sticky="EW", )
+        container.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
 
         for FrameClass in (MainPage, CharactersInfo, ArtifactInfo):
             frame = FrameClass(container, self)
@@ -52,18 +51,62 @@ class CharactersInfo(ttk.Frame):
             text="Back",
             command=lambda: controller.show_frame(MainPage)
         )
-        character_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+        character_button.grid(column=1, row=4, columnspan=2, sticky="NSEW")
 
 
 class ArtifactInfo(ttk.Frame):
     def __init__(self, container, controller):
         super().__init__(container)
+        self.controller = controller
         character_button = ttk.Button(
             self,
             text="Back",
             command=lambda: controller.show_frame(MainPage)
         )
         character_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+        # Create a label to display the selected artifact image
+        self.image_label = tk.Label(self)
+        self.image_label.grid()
+
+        # Call the show_relict_info method to initialize the dropdown menu
+        self.show_relict_info()
+
+    def show_relict_info(self):
+        artifacts = ["Band of Sizzling Thunder",
+                     "Champion of Streetwise Boxing",
+                     "Eagle of Twilight Line",
+                     "Firesmith of Lava-Forging",
+                     "Genius of Brilliant Stars",
+                     "Guard of Wuthering Snow",
+                     "Hunter of Glacial Forest",
+                     "Knight of Purity Palace",
+                     "Longevous Disciple",
+                     "Messenger Traversing Hackerspace",
+                     "Musketeer of Wild Wheat",
+                     "Passerby of Wandering Cloud",
+                     "Thief of Shooting Meteor",
+                     "Wastelander of Banditry Desert"]
+
+        var = tk.StringVar(self)
+        var.set(artifacts[0])  # Set the default artifact
+
+        option_menu = tk.OptionMenu(self, var, *artifacts)
+        option_menu.grid(pady=10)
+
+        var.trace("w", lambda *args: self.relict_info(var.get()))
+
+        # Call relict_info to initialize the image
+        self.relict_info(var.get())
+
+    def relict_info(self, selected_option):
+        image_filename = f"/Users/veronika/PycharmProjects/HonkaiHelper/image_relict/{selected_option}.png"
+        image = Image.open(image_filename)
+        image = image.resize((200, 200))  # Resize the image if needed
+        photo = ImageTk.PhotoImage(image)
+        self.image_label.config(image=photo)
+        self.image_label.image = photo  # Keep a reference to prevent garbage collection
+
 
 
 root = HonkaiHelper()
